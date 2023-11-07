0bfefe48f414f5a7683f1d1fbcedaee27e3ee0d9
Javier Alberto Herrera Pedreros <javierherrera8732@yahoo.com>
2023-11-07 09:35:47 -0500

    Update UsersController.cs

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,102 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Net.Http;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
 using Newtonsoft.Json.Linq;
 using dvcsharp_core_api.Models;
 using dvcsharp_core_api.Data;
-
 namespace dvcsharp_core_api
 {
    [Route("api/[controller]")]
-   public class UsersController : Controller
+   public class ImportsController : Controller
    {
       private readonly GenericDataContext _context;
-
-      public UsersController(GenericDataContext context)
+Remove this unread private field '_context' or refactor the code to use its value.
+      public ImportsController(GenericDataContext context)
       {
          _context = context;
       }
-
-      [Authorize]
-      [HttpGet]
-      public IEnumerable<User> Get()
+      [HttpPost]
+      public IActionResult Post()
       {
-         return _context.Users.ToList();
-      }
-
-      [Authorize]
-      [HttpPut("{id}")]
-      public IActionResult Put(int id, [FromBody] Models.UserUpdateRequest user)
-      {
-         if(!ModelState.IsValid) {
-            return BadRequest(ModelState);
+         XmlDocument xmlDocument = new XmlDocument();
+         xmlDocument.Load(HttpContext.Request.Body);
+         var entities = new List<Object>();
+         foreach(XmlElement xmlItem in xmlDocument.SelectNodes("Entities/Entity"))
+         {
+            string typeName = xmlItem.GetAttribute("Type");
+            if(String.IsNullOrEmpty(typeName))
+               continue;
+            //Console.WriteLine("Trying to deserialize: " + typeName);
+            //Console.WriteLine("Content: " + xmlItem.InnerXml);
+            var xser = new XmlSerializer(Type.GetType(typeName));
+            var reader = new XmlTextReader(new StringReader(xmlItem.InnerXml));
+            entities.Add(xser.Deserialize(reader));
          }
-
-         var existingUser = _context.Users.SingleOrDefault(m => m.ID == id);
-         if(existingUser == null) {
-            return NotFound();
-         }
-
-         existingUser.name = user.name;
-         existingUser.email = user.email;
-         existingUser.role = user.role;
-         existingUser.updatePassword(user.password);
-
-         _context.Users.Update(existingUser);
-         _context.SaveChanges();
-
-         return Ok(existingUser);
-      }
-
-      [Authorize]
-      [HttpDelete("{id}")]
-      public IActionResult Delete(int id)
-      {
-         User user = _context.Users.SingleOrDefault(m => m.ID == id);
-
-         if(user == null) {
-            return NotFound();
-         }
-
-         _context.Users.Remove(user);
-         _context.SaveChanges();
-
-         return Ok(user);
-      }
-
-      [Authorize]
-      [HttpGet("import")]
-      public async Task<IActionResult> Import()
-      {
-         HttpClient client = new HttpClient();
-         var url = HttpContext.Request.Query["url"].ToString();
-
-         string responseBody = null;
-         string errorMsg = "Success";
-
-         try {
-            HttpResponseMessage response = await client.GetAsync(url);
-            response.EnsureSuccessStatusCode();
-            responseBody = await response.Content.ReadAsStringAsync();
-
-            MockUserImport(responseBody);
-         }
-         catch(Exception e) {
-            errorMsg = e.Message;
-         }
-
-         return Ok(new JObject(
-            new JProperty("Error", errorMsg),
-            new JProperty("Content", responseBody)
-         ));
-      }
-
-      private void MockUserImport(string data)
-      {
-         // Mock
+         return Ok(entities);
       }
    }
 }
